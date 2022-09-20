c7ba35bcbf3b8a7317e5e77d26cff11681b123bc
Kasey <kasey_crocker@yahoo.com>
2022-09-20 19:16:21 -0400

    front end javascript issues

--- public/javascript/edit.js
+++ public/javascript/edit.js
@@ -2,13 +2,16 @@ async function editFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value.trim();
+    const calories = document.querySelector('input[name="post-calories"]').value;
+    
     const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
     const response = await fetch(`/api/posts/${id}`, {
       method: 'PUT',
       body: JSON.stringify({
-        title
+        title,
+        calories
       }),
       headers: {
         'Content-Type': 'application/json'
@@ -16,7 +19,7 @@ async function editFormHandler(event) {
     });
   
     if (response.ok) {
-      document.location.replace('/dashboard/');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
