128b3f462c4c0745a21dee4af91572cb126c01b1
rick Deakins <rdeakins96@gmail.com>
2022-09-20 20:44:21 -0400

    updated chart js

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -19,3 +19,18 @@ const myChart = new Chart(ctx, {
 
 });
 
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+function removeData(chart) {
+    chart.data.labels.pop();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    chart.update();
+}
