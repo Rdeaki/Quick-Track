3177a55e380f61076a6b6a27c79fa7c2250af1b8
Kasey <kasey_crocker@yahoo.com>
2022-09-20 20:35:40 -0400

    delete and add meal js

--- public/javascript/edit.js
+++ public/javascript/edit.js
@@ -2,11 +2,10 @@ async function editFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value.trim();
-    const calories = document.querySelector('input[name="post-calories"]').value;
+    const calories = document.querySelector('input[name="post-calories"]').value.trim();
     
-    const id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
+    const id = document.querySelector('.post-id').getAttribute('id');
+
     const response = await fetch(`/api/posts/${id}`, {
       method: 'PUT',
       body: JSON.stringify({
@@ -25,4 +24,6 @@ async function editFormHandler(event) {
     }
   }
   
-  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+  document.querySelector('.edit-post').addEventListener('submit', editFormHandler);
+
+  
